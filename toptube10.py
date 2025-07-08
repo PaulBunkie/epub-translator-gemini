917d2a18b07a693d396e33bb0c7ae44648e09a74
Paul Bunkie <paulbunkie@gmail.com>
2025-07-08 04:58:13 +0300

    Update query template and clean up CSS in toptube files
    - Expanded the Q_TEMPLATE constant in toptube10.py to include 'TheDiaryOfACEO', enhancing search capabilities for video content analysis.
    - Removed unused CSS styles from toptube.html to improve code clarity and maintainability.

--- toptube10.py
+++ toptube10.py
@@ -11,7 +11,7 @@ import video_analyzer
 
 # Константы
 DAYS = 3
-Q_TEMPLATE = "interview|интервью|беседа|обзор|разговор|репортаж|дудь|варламов|собчак|лебедев|rogan|tucker|Ferriss|Musk|редакция"
+Q_TEMPLATE = "interview|интервью|беседа|обзор|разговор|репортаж|дудь|варламов|собчак|лебедев|rogan|tucker|Ferriss|Musk|редакция|TheDiaryOfACEO"
 load_dotenv()
 API_KEY = os.getenv('YOUTUBE_API_KEY')
 
